c937e5dd0b319d1b2277ad68b7b46430d6bf318f
Nick Massey <nickmass@nickmass.com>
2018-03-02 18:27:26 -0700

    Print errors with greater detail

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate acme_client;
 extern crate clap;
-#[macro_use]
 extern crate failure;
 #[macro_use]
 extern crate failure_derive;
@@ -18,7 +17,7 @@ extern crate serde_json;
 use acme_client::Directory;
 use acme_client::openssl::pkey::PKey;
 use acme_client::openssl::x509::X509Req;
-use failure::Error;
+use failure::{Error, ResultExt};
 use rusoto_credential::StaticProvider;
 use rusoto_core::DispatchSignedRequest;
 use rusoto_route53::{Route53, Route53Client};
@@ -26,6 +25,18 @@ use rusoto_route53::{Route53, Route53Client};
 use std::fs::File;
 use std::io::Read;
 use std::path::{Path, PathBuf};
+
+#[derive(Fail, Debug)]
+enum AcmeError {
+    #[fail(display = "Route53 DNS update took longer than {} seconds to propigate and has timed out", _0)]
+    Route53PropigateTimeout(u64),
+    #[fail(display = "Route53 returned an invalid response while waiting for propigation: {}", _0)]
+    Route53InvalidChangeResponse(String),
+    #[fail(display = "Unable to get Acme DNS challenge")]
+    NoAcmeDnsChallenge,
+    #[fail(display = "Error returned from acme_client: {}", _0)]
+    AcmeClient(acme_client::error::ErrorKind)
+}
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 struct ConfigCertificate {
@@ -86,8 +97,25 @@ impl Config {
     }
 }
 
-
 fn main() {
+    let code = match run() {
+        Err(e) => {
+            for e in e.causes() {
+                error!("{}", e);
+            }
+            error!("{:?}", e);
+            1
+        }
+        _ => {
+            info!("Success");
+            0
+        }
+    };
+
+    std::process::exit(code)
+}
+
+fn run() -> Result<(), Error> {
     let mut builder = ::env_logger::Builder::new();
     builder.filter(None, ::log::LevelFilter::Info);
     builder.init();
@@ -97,12 +125,12 @@ fn main() {
     let config = args.next().unwrap_or(String::from("config.json"));
 
     info!("Loading config from: {}", config);
-    let config = load_config(&config).expect(&format!("Unable to load config: {}", config));
+    let config = load_config(&config).context(format!("Unable to load config: {}", config))?;
     info!("Config loaded");
 
-    let dns_client = DnsClient::new(&config).expect("Could not create route53 client");
+    let dns_client = DnsClient::new(&config).context("Could not create route53 client")?;
 
-    let _ = std::fs::create_dir_all(&*config.certificate_directory).expect("Count not create SSL directory");
+    let _ = std::fs::create_dir_all(&*config.certificate_directory).context("Could not create SSL directory")?;
 
     let dir_url = &*config.directory_url
         .as_ref()
@@ -110,31 +138,35 @@ fn main() {
         .unwrap_or(acme_client::LETSENCRYPT_DIRECTORY_URL);
     info!("Creating ACME directory: {}", dir_url);
     let dir = Directory::from_url(dir_url)
-        .expect("Unable to create ACME Directory");
+        .map_err(|e|AcmeError::AcmeClient(e.0))
+        .context("Unable to create ACME Directory")?;
 
     let account = {
         let account_key_path = config.account_key_path();
         info!("Checking for account key in: {}", account_key_path.display());
         if let Some(account_key) = load_key(&account_key_path)
-            .expect("Could not open account key") {
+            .context("Could not open account key")? {
                 info!("Account key found");
                 info!("Registering account");
                 dir.account_registration()
                     .email(&*config.email_address)
                     .pkey(account_key)
                     .register()
-                    .expect("Unable to register account")
+                    .map_err(|e|AcmeError::AcmeClient(e.0))
+                    .context("Unable to register account")?
             } else {
                 info!("No account key found, generating");
                 info!("Registering account");
                 let account = dir.account_registration()
                     .email(&*config.email_address)
                     .register()
-                    .expect("Unable to register account and generate account key");
+                    .map_err(|e|AcmeError::AcmeClient(e.0))
+                    .context("Unable to register account and generate account key")?;
 
                 info!("Storing account key in: {}", account_key_path.display());
                 account.save_private_key(account_key_path)
-                    .expect("Unable to save account key");
+                    .map_err(|e|AcmeError::AcmeClient(e.0))
+                    .context("Unable to save account key")?;
 
                 account
             }
@@ -145,30 +177,35 @@ fn main() {
         for domain in &cert.domains {
             info!("Begin authenticating: {}", domain);
             let auth = account.authorization(domain)
-                .expect(&*format!("Unable to create auhtorization for: {}", domain));
+                .map_err(|e|AcmeError::AcmeClient(e.0))
+                .context(format!("Unable to create auhtorization for: {}", domain))?;
 
             info!("Getting DNS challenge");
             let challenge = auth
                 .get_dns_challenge()
-                .expect(&*format!("Unable to get DNS challenge for: {}", domain));
+                .ok_or(AcmeError::NoAcmeDnsChallenge)
+                .context(format!("Unable to get DNS challenge for: {}", domain))?;
 
             let dns_name = format!("_acme-challenge.{}", domain);
             let dns_value = challenge
                 .signature()
-                .expect(&*format!("Unable to get DNS signature for: {}", domain));
+                .map_err(|e|AcmeError::AcmeClient(e.0))
+                .context(format!("Unable to get DNS signature for: {}", domain))?;
 
             info!("Setting challenge record on: {}", dns_name);
             dns_client.set_dns_record(&*dns_name, &*dns_value)
-                .expect(&*format!("Unable to set DNS record: {}", dns_name));;
+                .context(format!("Unable to set DNS record: {}", dns_name))?;
 
             info!("Validating domain: {}", domain);
-            challenge.validate().expect(&*format!("Validation failed for: {}", domain));
+            challenge.validate()
+                .map_err(|e|AcmeError::AcmeClient(e.0))
+                .context(format!("Validation failed for: {}", domain))?;
         }
 
         let cert_dir = cert.directory(&*config.certificate_directory);
         info!("Creating certificate directory: {}", cert_dir.display());
         let _ = std::fs::create_dir_all(cert_dir)
-            .expect(&*format!("Unable to create cert directory for: {}", cert.name));
+            .context(format!("Unable to create cert directory for: {}", cert.name))?;
 
         let domains: Vec<_> = cert.domains.iter().map(String::as_str).collect();
         let mut cert_signer = account.certificate_signer(&*domains);
@@ -178,7 +215,7 @@ fn main() {
         let csr_file = cert.csr_path(&*config.certificate_directory);
         info!("Checking for existing CSR in: {}", csr_file.display());
         if let Some(csr) = load_csr(&csr_file)
-            .expect(&*format!("Unable to read csr for: {}", cert.name)) {
+            .context(format!("Unable to read csr for: {}", cert.name))? {
                 info!("Existing CSR found");
                 cert_signer = cert_signer.csr(csr);
                 write_csr = false;
@@ -189,7 +226,7 @@ fn main() {
         let key_file = cert.key_path(&*config.certificate_directory);
         info!("Checking for existing key in: {}", key_file.display());
         if let Some(key) = load_key(&key_file)
-            .expect(&*format!("Unable to read key for: {}", cert.name)) {
+            .context(format!("Unable to read key for: {}", cert.name))? {
                 info!("Existing key found");
                 cert_signer = cert_signer.pkey(key);
                 write_key = false;
@@ -199,18 +236,21 @@ fn main() {
 
         info!("Signing cert");
         let signed_cert = cert_signer.sign_certificate()
-            .expect(&*format!("Unable to sign certificate for: {}", cert.name));
+            .map_err(|e|AcmeError::AcmeClient(e.0))
+            .context(format!("Unable to sign certificate for: {}", cert.name))?;
 
         if write_csr {
             info!("Saving CSR to: {}", csr_file.display());
             signed_cert.save_csr(cert.csr_path(&*config.certificate_directory))
-                .expect(&*format!("Unable to save CSR for: {}", cert.name));
+                .map_err(|e|AcmeError::AcmeClient(e.0))
+                .context(format!("Unable to save CSR for: {}", cert.name))?;
         }
 
         if write_key {
             info!("Saving key to: {}", key_file.display());
             signed_cert.save_private_key(key_file)
-                .expect(&*format!("Unable to save Key for: {}", cert.name));
+                .map_err(|e|AcmeError::AcmeClient(e.0))
+                .context(format!("Unable to save Key for: {}", cert.name))?;
         }
 
         let cert_file = cert.cert_path(&*config.certificate_directory);
@@ -219,10 +259,11 @@ fn main() {
                                                       .as_ref()
                                                       .map(String::as_str),
                                                       cert_file)
-            .expect(&*format!("Unable to save Certificate for: {}", cert.name));
+            .map_err(|e|AcmeError::AcmeClient(e.0))
+            .context(format!("Unable to save Certificate for: {}", cert.name))?;
     }
 
-    info!("Success");
+    Ok(())
 }
 
 fn load_config<P: AsRef<Path>>(path: P) -> Result<Config, Error> {
@@ -337,13 +378,13 @@ impl DnsClient {
             match &*res.change_info.status {
                 "INSYNC" => return Ok(()),
                 "PENDING" => (),
-                s @  _ => bail!("Invalid response from Route53 get_change: {}", s),
+                s @  _ => Err(AcmeError::Route53InvalidChangeResponse(s.to_string()))?,
             }
 
             info!("DNS update still pending, retrying in {} seconds", next_delay);
 
             total_time -= next_delay;
-            if total_time < 0 { bail!("Ran out of time waiting for route53 to propigate") }
+            if total_time < 0 { Err(AcmeError::Route53PropigateTimeout(self.config.aws_max_wait))? }
             std::thread::sleep(std::time::Duration::from_secs(next_delay as u64));
         }
     }
